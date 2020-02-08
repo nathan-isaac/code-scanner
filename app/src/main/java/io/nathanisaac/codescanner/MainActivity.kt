61f2d74ad7f9796a796361bab5c4f7a104617bde
Nathan Isaac <nathanjisaac@gmail.com>
2020-02-08 15:53:34 -0800

    Play with adding images from camera

--- app/src/main/java/io/nathanisaac/codescanner/MainActivity.kt
+++ app/src/main/java/io/nathanisaac/codescanner/MainActivity.kt
@@ -1,11 +1,16 @@
 package io.nathanisaac.codescanner
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.FirebaseApp
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 
@@ -15,11 +20,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        FirebaseApp.initializeApp(this)
-
         val textView = findViewById<TextView>(R.id.textValue)
         val readCodeButton = findViewById<Button>(R.id.getCode)
         val barcodeImage = findViewById<ImageView>(R.id.barcodeImage)
+
+        FirebaseApp.initializeApp(this)
 
         val imageBitmap: Bitmap = BitmapFactory.decodeResource(this.resources, R.drawable.barcode_generator)
         val image: FirebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap)
@@ -27,26 +32,90 @@ class MainActivity : AppCompatActivity() {
         val codeReader: CodeReader = CodeReader()
 
         readCodeButton.setOnClickListener {
+//            codeReader.getImageBarcodes(image) { codes ->
+//                for (code in codes) {
+//                    val bounds = code.boundingBox
+//                    val corners = code.cornerPoints
+//                    val rawValue = code.rawValue
+//                    val valueType = code.valueType
+//                    if (bounds !== null) {
+//                        val imageCopy = imageBitmap.copy(Bitmap.Config.ARGB_8888, true)
+//                        val canvas = Canvas(imageCopy)
+//                        val paint = Paint()
+//                        val rectangle = Rect(bounds.left, bounds.top, bounds.right, bounds.bottom)
+//
+//                        paint.color = Color.RED
+//                        paint.style = Paint.Style.STROKE
+//                        paint.strokeWidth = 10.toFloat()
+//
+//                        canvas.drawRect(rectangle, paint)
+//
+//                        barcodeImage.setImageBitmap(imageCopy)
+//                    }
+
+//                    textView.text = code.rawValue.toString()
+//                }
+//            }
+
+            dispatchTakePictureIntent()
+
+
+//            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+//                showCameraPerview()
+//            } else {
+//                // Camera permission has not been granted.
+//
+//                if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+//                    Toast.makeText(this, "Camera permission is needed to show the camera preview.", Toast.LENGTH_SHORT).show()
+//                }
+//
+//                requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA)
+//            }
+
+
+        }
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val codeReader: CodeReader = CodeReader()
+        val barcodeImage = findViewById<ImageView>(R.id.barcodeImage)
+        val textView = findViewById<TextView>(R.id.textValue)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            barcodeImage.setImageBitmap(imageBitmap)
+            val image: FirebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap)
+
             codeReader.getImageBarcodes(image) { codes ->
                 for (code in codes) {
-                    val bounds = code.boundingBox
-                    val corners = code.cornerPoints
-                    val rawValue = code.rawValue
-                    val valueType = code.valueType
-                    if (bounds !== null) {
-                        val imageCopy = imageBitmap.copy(Bitmap.Config.ARGB_8888, true)
-                        val canvas = Canvas(imageCopy)
-                        val paint = Paint()
-                        val rectangle = Rect(bounds.left, bounds.top, bounds.right, bounds.bottom)
-
-                        paint.color = Color.RED
-                        paint.style = Paint.Style.STROKE
-                        paint.strokeWidth = 10.toFloat()
-
-                        canvas.drawRect(rectangle, paint)
-
-                        barcodeImage.setImageBitmap(imageCopy)
-                    }
+//                    val bounds = code.boundingBox
+//                    val corners = code.cornerPoints
+//                    val rawValue = code.rawValue
+//                    val valueType = code.valueType
+//                    if (bounds !== null) {
+//                        val imageCopy = imageBitmap.copy(Bitmap.Config.ARGB_8888, true)
+//                        val canvas = Canvas(imageCopy)
+//                        val paint = Paint()
+//                        val rectangle = Rect(bounds.left, bounds.top, bounds.right, bounds.bottom)
+//
+//                        paint.color = Color.RED
+//                        paint.style = Paint.Style.STROKE
+//                        paint.strokeWidth = 10.toFloat()
+//
+//                        canvas.drawRect(rectangle, paint)
+//
+//                        barcodeImage.setImageBitmap(imageCopy)
+//                    }
 
                     textView.text = code.rawValue.toString()
                 }
