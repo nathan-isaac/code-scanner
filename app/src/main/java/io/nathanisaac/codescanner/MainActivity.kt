2d1f7017e04032b68271d15e7b55c2d6e2e7da79
Nathan Isaac <nathanjisaac@gmail.com>
2020-02-07 18:27:10 -0800

    Add simple button to change text value

--- app/src/main/java/io/nathanisaac/codescanner/MainActivity.kt
+++ app/src/main/java/io/nathanisaac/codescanner/MainActivity.kt
@@ -2,11 +2,22 @@ package io.nathanisaac.codescanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textValue)
+        val readCodeButton = findViewById<Button>(R.id.getCode)
+
+        textView.text = "this is a test"
+
+        readCodeButton.setOnClickListener {
+            textView.text = "Button Click"
+        }
     }
 }
